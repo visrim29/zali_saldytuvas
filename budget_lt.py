8084d350436ef52495986285d07a1f0a4f0539ec
visrim29 <r.visagurskis@gmail.com>
2024-01-04 15:54:23 +0200

    budget

--- budget_lt.py
+++ budget_lt.py
@@ -1,16 +1,3 @@
-""" Komandinio darbo / savarankiška užduotis
-===[ Biudžetas ]===
-
-Reikalavimai
-
-* Biudžeto turinys - pajamų/išlaidų žurnalo žodynas
-** raktas - paskirtis
-** reikšmė - pajamos pozityvus float, išlaidos negatyvus float
-* Galimybė pridėti pajamas arba išlaidas
-* Spausdinti pajamų/išlaidų žurnalą
-* Suskaičiuoti biudžeto balansą
-
-"""
 budget = {}
 
 def budget_add(purpose, amount):
